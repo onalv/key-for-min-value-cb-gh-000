c32d817bd2554016b6c9fb3c2656fc1f91c54496
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-14 16:31:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,16 @@
 def key_for_min_value(name_hash)
   values_array = name_hash.collect{|key, value| value}
   min_value = values_array[0]
-  values_array.each{|number| number < min_value ? min_value = number : min_value}
-  min_value
+  key_min = ""
+  name_hash.collect do |key, value|
+    if value < min_value
+      key_min = key
+      min_value = value
+    end
+  end
+  if key_min != ""
+    key_min
+  else
+    nil
+  end
 end
