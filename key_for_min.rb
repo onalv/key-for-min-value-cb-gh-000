b5916a924678560c14765c0fdc9c0303d3e4081f
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-14 16:30:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,7 @@
 
 def key_for_min_value(name_hash)
   values_array = name_hash.collect{|key, value| value}
+  min_value = values_array[0]
+  values_array.each{|number| number < min_value ? min_value = number : min_value}
+  min_value
 end
